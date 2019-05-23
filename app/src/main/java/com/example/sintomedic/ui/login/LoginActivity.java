57fe4892432c3040bfb83b0d7c032f73f7e4afde
Sergio M <38441398+Sergiotmg@users.noreply.github.com>
2019-05-23 20:59:07 +0200

    login segun tipo user 2

--- app/src/main/java/com/example/sintomedic/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/sintomedic/ui/login/LoginActivity.java
@@ -23,8 +23,11 @@ import android.widget.Toast;
 import com.example.sintomedic.DoctorRegisterActivity;
 import com.example.sintomedic.PacienteRegisterActivity;
 import com.example.sintomedic.R;
-import com.example.sintomedic.ui.login.LoginViewModel;
-import com.example.sintomedic.ui.login.LoginViewModelFactory;
+import com.example.sintomedic.UnsafeOkHttpClient;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -137,6 +140,8 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+
+
     private void updateUiWithUser(LoggedInUserView model) {
         String welcome = getString(R.string.welcome) + model.getDisplayName();
         // TODO : initiate successful logged in experience
@@ -147,7 +152,68 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * Method that performs RESTful webservice invocations
+     *
+     * @param params
+     */
 
+    /*
+    public void invokeWS(RequestParams params){
+        // Show Progress Dialog
+        prgDialog.show();
+        // Make RESTful webservice call using AsyncHttpClient object
+        UnsafeOkHttpClient client = new UnsafeOkHttpClient();
+        client.get("http://192.168.2.2:9999/useraccount/login/dologin",params ,new UnsafeOkHttpClient() {
+            // When the response returned by REST has Http response code '200'
+            @Override
+            public void onSuccess(String response) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                try {
+                    // JSON Object
+                    JSONObject obj = new JSONObject(response);
+                    // When the JSON response has status boolean value assigned with true
+                    if(obj.getBoolean("status")){
+                        Toast.makeText(getApplicationContext(), "You are successfully logged in!", Toast.LENGTH_LONG).show();
+                        // Navigate to Home screen
+                        goToUserActivity();
+                    }
+                    // Else display error message
+                    else{
+                        errorMsg.setText(obj.getString("error_msg"));
+                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+
+                }
+            }
+            // When the response returned by REST has Http response code other than '200'
+            @Override
+            public void onFailure(int statusCode, Throwable error,
+                                  String content) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                // When Http response code is '404'
+                if(statusCode == 404){
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code is '500'
+                else if(statusCode == 500){
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code other than 404, 500
+                else{
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+        */
 
     // actividad de registro paciente
     public void goToPacienteRegisterActivity(View view) {
